d347a2cd93b0f82bedccc8bed0aacf769dd2aac7
Chathu Vishwajith <4evercreations@gmail.com>
2019-06-10 12:47:50 +0530

    Add provider

---
+++ theme/Provider.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Main } from '../Layout';
+
+const Provider = (props) => <Main withFooter {...props} />;
+
+export default Provider;
--- theme/index.js
+++ theme/index.js
@@ -1,19 +1,28 @@
+import Provider from './Provider';
+
 export default {
   font: 'Source Sans Pro, sans-serif',
-  fontWeight: 400,
+  monospace: 'IBM Plex Mono, monospace',
+  fontWeight: 300,
   colors: {
-    text: '#fff',
-    background: '#282c35',
+    text: '#000',
+    background: 'rgb(249, 249, 251)',
   },
   heading: {
-    fontFamily: `'Source Sans Pro', monospace`,
+    fontFamily: `'Source Sans Pro', Provider`,
     fontWeight: 700,
-  },
-  h1: {
+    margin: 0,
     color: 'rgb(0, 0, 0)',
   },
+  h2: {
+    fontWeight: 400,
+  },
+  p: {
+    margin: 0,
+  },
   googleFont:
-    'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700&display=swap',
+    'https://fonts.googleapis.com/css?family=IBM+Plex+Mono|Source+Sans+Pro:300,400,700&display=swap',
+  Provider,
 };
 
 export const dark = {};
