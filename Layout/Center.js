af5652695e6c2462a134c0e473fb2b7ed078a5da
Chathu Vishwajith <4evercreations@gmail.com>
2019-06-10 12:15:23 +0530

    Update Center Layout with option for footer and add proptypes

--- Layout/Center.js
+++ Layout/Center.js
@@ -2,17 +2,40 @@
 import { jsx, css } from '@emotion/core';
 import PropTypes from 'prop-types';
 
-const Center = ({ children }) => {
+import Footer from './Footer';
+
+const Center = ({ children, withFooter, eventName, twitterHandle }) => {
   return (
     <div
       css={css`
         display: flex;
+        width: 100%;
+        height: 90%;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
       `}>
       {children}
+      {withFooter ? (
+        <Footer>
+          <span>{eventName} </span>
+          <span>{twitterHandle}</span>
+        </Footer>
+      ) : null}
     </div>
   );
+};
+
+Center.defaultProps = {
+  withFooter: false,
+  eventName: 'EventName 🇱🇰',
+  twitterHandle: '@iamChathu',
+};
+
+Center.propTypes = {
+  withFooter: PropTypes.bool,
+  eventName: PropTypes.string,
+  twitterHandle: PropTypes.string,
 };
 
 export default Center;
